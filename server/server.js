49411acbac4afaab6c0468e8de11c762993bbd0b
Priyansh <priyansht1999@gmail.com>
2021-06-10 17:39:55 +0530

    env_file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
@@ -7,14 +8,14 @@ const SpotifyWebApi = require("spotify-web-api-node")
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.post("/refresh", (req, res) => {
     const refreshToken = req.body.refreshToken
-    // console.log("hi")
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: 'a870774ba0f54cca924fba0d0a4f23f1',
-        clientSecret: '857ac871015149f480d66b7b660310e3',
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
         refreshToken
     })
 
