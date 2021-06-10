0d187e02ae5809cf26741eb194c89513aeeb584d
Priyansh <priyansht1999@gmail.com>
2021-06-10 17:29:16 +0530

    Spotify2.0

--- server/server.js
+++ server/server.js
@@ -1,18 +1,44 @@
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const lyricsFinder = require("lyrics-finder")
 const SpotifyWebApi = require("spotify-web-api-node")
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken
+    // console.log("hi")
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: 'a870774ba0f54cca924fba0d0a4f23f1',
+        clientSecret: '857ac871015149f480d66b7b660310e3',
+        refreshToken
+    })
+
+    spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        refreshToken: data.body.refreshToken,
+        expiresIn: data.body.expiresIn,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
 app.post('/login',(req,res)=>{
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
         redirectUri: 'http://localhost:3000',
-        clientId: ' a870774ba0f54cca924fba0d0a4f23f1',
-        clientSecret: ' 857ac871015149f480d66b7b660310e3'
+        clientId: 'a870774ba0f54cca924fba0d0a4f23f1',
+        clientSecret: '857ac871015149f480d66b7b660310e3'
 
     })
 
@@ -26,10 +52,16 @@ app.post('/login',(req,res)=>{
             expiresIn: data.body.expires_in
         })
     })
-    .catch(() =>{
-        console.log(error)
+    .catch(err => {
+        console.log(err)
         res.sendStatus(400)
-    })
+      })
+})
+
+app.get('/lyrics',async(req,res)=>{
+    const lyrics = await lyricsFinder(req.query.artist, req.query.track) || 
+    "No Lyrics Found"
+    res.json({lyrics})
 })
 
 app.listen(3001)
